6ef80f76bb7f36c9721eb68e89221379a3faf9ca
saifulaija <saifulaija2023@gmail.com>
2024-05-29 20:26:05 +0600

    edit done

--- src/app/(withDashboardLayout)/dashboard/admin/flat_management/components/column.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/flat_management/components/column.tsx
@@ -1,14 +1,11 @@
-
-
-
-
-
-
-"use client"
+"use client";
 
 import { ColumnDef } from "@tanstack/react-table";
 import Image from "next/image";
 import DeleteFlat from "./deleteFlat";
+import MyDialog from "@/components/ShadCn/MyDialog";
+import { Button } from "@/components/ui/button";
+import FlatUpdateForm from "@/components/Form/FlatUpdateForm";
 
 // This type is used to define the shape of our data.
 export type Flat = {
@@ -16,7 +13,10 @@ export type Flat = {
   location: string;
   rentAmount: string;
   advanceAmount: string;
+  bedRooms: string;
   space: string;
+  amenities:string;
+  description:string;
   image: { id: string; url: string; flatId: string }[];
   created: string;
 };
@@ -27,7 +27,7 @@ export const columns: ColumnDef<Flat>[] = [
     header: "Image",
     cell: ({ row }) => {
       const images = row.original.image;
-      const profilePhoto = images.length > 0 ? images[0].url : '/logo';
+      const profilePhoto = images.length > 0 ? images[0].url : "/logo";
       return (
         <Image
           src={profilePhoto}
@@ -56,38 +56,7 @@ export const columns: ColumnDef<Flat>[] = [
     header: "Space",
   },
 
-//   {
-//     id: "actions",
-//     cell: ({ row }) => {
-//       const payment = row.original
  
-//       return (
-//         <DropdownMenu>
-//           <DropdownMenuTrigger asChild>
-//             <Button variant="ghost" className="h-8 w-8 p-0">
-//               <span className="sr-only">Open menu</span>
-//               <MoreHorizontal className="h-4 w-4" />
-//             </Button>
-//           </DropdownMenuTrigger>
-//           <DropdownMenuContent align="end">
-//             <DropdownMenuLabel>Actions</DropdownMenuLabel>
-//             <DropdownMenuItem
-//               onClick={() => navigator.clipboard.writeText(payment.id)}
-//             >
-//               Copy payment ID
-//             </DropdownMenuItem>
-//             <DropdownMenuSeparator />
-//             <DropdownMenuItem>View customer</DropdownMenuItem>
-//             <DropdownMenuItem>View payment details</DropdownMenuItem>
-//           </DropdownMenuContent>
-//         </DropdownMenu>
-//       )
-//     },
-//   },
-
-
-
-  // Uncomment and implement these if you need user status and role change functionality
   {
     id: "statusActions",
     header: "Action",
@@ -97,14 +66,18 @@ export const columns: ColumnDef<Flat>[] = [
     },
   },
 
-
-  // {
-  //   id: "roleActions",
-  //   header: "Change Role",
-  //   cell: ({ row }) => {
-  //     const user = row.original;
-  //     return <UpdateUserRole userId={user.id} currentStatus={user.role} />;
-  //   },
-  // },
+  {
+    id: "editActions",
+    header: "Action",
+    cell: ({ row }) => {
+      const flat = row.original;
+      return (
+        <div className="my-10">
+          <MyDialog triggerButton={<Button variant="outline">Edit</Button>}>
+            <FlatUpdateForm data={flat} />
+          </MyDialog>
+        </div>
+      );
+    },
+  },
 ];
-
